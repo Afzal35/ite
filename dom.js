72d69a267f61856eebed9fa469578e38dad2de7a
Afzal Alam <101258058+Afzal35@users.noreply.github.com>
2022-03-13 11:34:50 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -42,3 +42,19 @@ var input = document.querySelector('input');
 input.value= 'Hello World'
 var submit = document.querySelector('input[type = "submit"]');
 submit.value= 'send'
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+var lastitem = document.querySelector('.list-group-item:last-child');
+lastitem.style.color = 'blue';
+var seconditem = document.querySelector('.list-group-item:nth-child(2)');
+seconditem.style.color = 'coral';
+
+var title = document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent = 'HEllo';
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i=0; i<odd.length; i++)
+{
+    odd[i].style.backgroundColor='#f4f4f4';
+    even[i].style.backgroundColor= '#ccc';
+}
